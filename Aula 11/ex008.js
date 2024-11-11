a239d0bbf50dbb04736d9ecd4c12b26ff162bd58
Guilherme Feitosa Monteiro <guinhofeitosa1@gmail.com>
2024-11-11 12:19:08 -0300

    continuação das aulas de js

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){ // Condição Simples
+    console.log('Você ultrapassou a velocidade permitida!')
+}
+console.log('Dirija sem com o cinto de segurança')
+
---
+++ Aula 11/ex009.js
@@ -0,0 +1,9 @@
+var nac = 'Brasil'
+console.log(`Nasci em ${nac}`)
+
+
+if (nac == 'Brasil'){
+    console.log('Você é de casa pai') 
+} else {
+    console.log('Seja bem-vindo gringo')
+}
