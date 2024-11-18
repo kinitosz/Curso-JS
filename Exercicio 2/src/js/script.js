018544155c72a672d7af0663b7207d01a3ea2f20
Guilherme Feitosa Monteiro <guinhofeitosa1@gmail.com>
2024-11-18 10:23:19 -0300

    continuação dos meus estudos de js

--- Exercicio 2/src/js/script.js
+++ Exercicio 2/src/js/script.js
@@ -1,33 +1,71 @@
-var valid = document.querySelector('#valid')
-var agora = new Date()
-var year = agora.getFullYear()
-var imgBox = document.getElementById('image')
-var img = document.querySelector('#image > img')
-var txt = document.querySelector('#txt')
-
 function validar(){
 
+    var data = new Date()
+    var year = data.getFullYear()
+    var imgBox = document.getElementById('image')
+    var img = document.querySelector('#image > img')
+    var txt = document.querySelector('#txt')
     var txtNas =  document.querySelector('#anoNas')
     var anoNas = Number(txtNas.value)
-    var idade = year - anoNas
-    txt.innerHTML = `Você tem ${idade} anos de idade!`
-    imgBox.removeAttribute('style', 'display: none')
 
-    if (idade < 6){
-        img.src = 'src/image/bebemenina.jpg'
+    if(anoNas == 0 || anoNas > year){
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else{
+        
+        var idade = year - anoNas
+        var sex = document.getElementsByName('radsex')
+        var genero = ''
+        if(sex[0].checked){
+            genero = 'Homem'
+        } else if(sex[1].checked){
+            genero = 'Mulher'
+        }
 
-    } else if(idade >= 7 && idade <= 12){
-        img.src = 'src/image/criancamenina.jpg'
+        if (idade < 6 && genero == 'Mulher'){
+            img.src = 'src/image/bebemenina.jpg'
+            txt.innerHTML = `Você é uma bebê de ${idade} anos de idade!`
 
-    } else if(idade > 12 && idade <= 18){
-        img.src = 'src/image/menina.jpg'
+        }else if(idade < 6 && genero == 'Homem'){
+            img.src = 'src/image/bebemenino.jpg'
+            txt.innerHTML = `Você é um bebê de ${idade} anos de idade!`
 
-    } else if(idade > 18 && idade <= 60){
-        img.src = 'src/image/adulta.jpg'
+        }else if(idade >= 7 && idade <= 12 && genero == 'Mulher'){
+            img.src = 'src/image/criancamenina.jpg'
+            txt.innerHTML = `Você é uma criança de ${idade} anos de idade!`
+    
+        }else if(idade >= 7 && idade <= 12 && genero == 'Homem'){
+            img.src = 'src/image/criancamenino.jpg'
+            txt.innerHTML = `Você é uma criança de ${idade} anos de idade!`
+    
+        }else if(idade > 12 && idade <= 18 && genero == 'Mulher'){
+            img.src = 'src/image/menina.jpg'
+            txt.innerHTML = `Você é uma adolescente de ${idade} anos de idade!`
+    
+        }else if(idade > 12 && idade <= 18 && genero == 'Homem'){
+            img.src = 'src/image/menino.jpg'
+            txt.innerHTML = `Você é um adolescente de ${idade} anos de idade!`
+    
+        }else if(idade > 18 && idade <= 60 && genero == 'Mulher'){
+            img.src = 'src/image/adulta.jpg'
+            txt.innerHTML = `Você é uma mulher de ${idade} anos de idade!`
+    
+        }else if(idade > 18 && idade <= 60 && genero == 'Homem'){
+            img.src = 'src/image/adulto.jpg'
+            txt.innerHTML = `Você é um homem de ${idade} anos de idade!`
+    
+        }else if(idade > 60 && genero == 'Mulher'){
+            img.src = 'src/image/idosa.jpg'
+            txt.innerHTML = `Você é uma senhora de ${idade} anos de idade!`
+    
+        }else if(idade > 60 && genero == 'Homem'){
+            img.src = 'src/image/idoso.jpg'
+            txt.innerHTML = `Você é um senhor de ${idade} anos de idade!`
+    
+        }
 
-    } else {
-        img.src = 'src/image/idosa.jpg'
-
+        
+        imgBox.removeAttribute('style', 'display: none')
     }
 
+    
 }
